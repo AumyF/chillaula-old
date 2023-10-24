d9b863e67d5297460de26f2dd861f266a01058b8
Aumy <aumy@outlook.jp>
2023-10-25 07:37:15 +0900

    TODO

--- app/_components/button.tsx
+++ app/_components/button.tsx
@@ -1,6 +1,7 @@
 import { css } from "@/_styled-system/css";
 import { Button as AriaButton } from "@ariakit/react";
 
+// TODO よく見たら実装がベッタリすぎる
 export const Button: React.FC<
   Omit<React.ComponentProps<typeof AriaButton>, "className">
 > = (props) => {
